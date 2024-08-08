b8191c0cf4efa48bab20792c06844d87217081e9
LuciZR <151607296+LuciZR@users.noreply.github.com>
2024-08-09 03:49:30 +0530

    𝑳𝒖𝒄𝒊𝒁𝑹 🥱

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const DATABASE_URL =
 module.exports = {
   VERSION: require('./package.json').version,
   SESSION_ID: (process.env.SESSION_ID || '').trim(),
-  DATABASE:
+  DATABASE: 'postgresql://zr_user:y7KKnslpelMrCP5DYhrMCDU2A3lFqWV6@dpg-cqqi13d6l47c73av3gp0-a.oregon-postgres.render.com/zr',
     DATABASE_URL === databasePath
       ? new Sequelize({
           dialect: 'sqlite',
@@ -27,12 +27,12 @@ module.exports = {
           },
           logging: false,
         }),
-  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  HANDLERS: (process.env.PREFIX || 'null').trim(),
+  SUDO: process.env.SUDO || '918293838182,916296851422',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LyFE',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '+918293838182,LuciZR',
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
   LOG_MSG: toBool(process.env.LOG_MSG) || false,
   RMBG_KEY: process.env.RMBG_KEY || 'null',
@@ -73,10 +73,10 @@ module.exports = {
     process.env.WARN_RESET_MESSAGE || `WARN RESET\nUser : &mention\nRemaining : &remaining`,
   WARN_KICK_MESSAGE: process.env.WARN_KICK_MESSAGE || '&mention kicked',
   TRUECALLER: process.env.TRUECALLER,
-  DELETE_TYPE: (process.env.DELETE_TYPE || '').trim(),
+  DELETE_TYPE: (process.env.DELETE_TYPE || 'pm').trim(),
   LIST_TYPE: (process.env.LIST_TYPE || 'text').trim(),
-  BING_COOKIE: (process.env.BING_COOKIE || '').trim(),
-  GEMINI_API_KEY: (process.env.GEMINI_API_KEY || '').trim(),
+  BING_COOKIE: (process.env.BING_COOKIE || '_C_Auth=; ipv6=hit=1721430070187&t=6; GI_FRE_COOKIE=gi_prompt=2&gi_fre=2; MUID=193F318A379B63E629572515364E62B6; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=6A0DA0748274400AA4AF7138566EFBCA&dmnchg=1; _SS=SID=01D44CF6445D6A203E0F583745B86B38; _clck=f5mxos%7C2%7Cfnl%7C0%7C1661; SRCHUSR=DOB=20240627&POEX=W; ANON=A=44C3814B8160FA85B09C878EFFFFFFFF&E=1e05&W=1; NAP=V=1.9&E=1dab&C=go2hIgadRMfTNjG_3GKSLU-ZaCSLGJBmmUZRy-FAzlEuMBz61W2b7A&W=1; PPLState=1; _U=1CWvjSanDBDwmKmYXgGsVpbYqgvVeYHMiP5-VCclpSeJxWMazB74p3YA1yvjfXX8s8y_d7v7QVeZ1c5QBuSdvevnFzOOZeVutmtht167d7W5qq3T2Je6ZgQP36G0xXj1f49RXXuzE_lR-cQqI8IwjuQunW7jvR-0JD6BARVctYlaZWI8NZPIaXCwcxIDs9JF8nM7bWII6dUGPCLZL_0kZoA; WLS=TS=63857023271&C=f0f4cc6daebd97be&N=Anowar; GI_FRE_COOKIE=gi_prompt=2&gi_fre=1; _clsk=9azlau%7C1721426545830%7C2%7C0%7Cy.clarity.ms%2Fcollect; SRCHHPGUSR=SRCHLANG=en&DM=1&CW=521&CH=1032&SCW=521&SCH=1032&BRW=MW&BRH=MT&DPR=2.1&UTC=330&HV=1721426544&PV=11.0.0&PRVCW=521&PRVCH=985').trim(),
+  GEMINI_API_KEY: (process.env.GEMINI_API_KEY || 'AIzaSyBzo5z_Uo4vQX5-hM-UDtRhUmiQqfH5ukc').trim(),
   ADMINS: process.env.GROUP_ADMINS || '',
   RENDER_NAME: (process.env.RENDER_NAME || '').trim(),
   RENDER_API_KEY: (process.env.RENDER_API_KEY || '').trim(),
